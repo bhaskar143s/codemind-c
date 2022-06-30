27b369a825ee28963bf5abc675d6ab7dc0f087c5
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-06-30 11:03:05 +0530

    #include<stdio.h> int main() {     int n,a[100],s[100];     scanf("%d",&n);     int i,sum=0,x,b,j=0,min=0;
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
        for(i=0;i<n;i++)
        {
            if(a[i]%2==0)
            {
                break;
            }
            else
            {
                sum+=a[i];
            }
        }
        printf("%d",sum);
    }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],s[100];
+    scanf("%d",&n);
+    int i,sum=0,x,b,j=0,min=0;
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            break;
+        }
+        else
+        {
+            sum+=a[i];
+        }
+    }
+    printf("%d",sum);
+}
