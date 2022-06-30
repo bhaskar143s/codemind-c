8b55a90dcd631944c352fe1a889c8607699758f5
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-06-30 19:12:45 +0530

    #include <stdio.h> #include <string.h> int main() {     char s[200];     int count = 0,i,c=0;     scanf("%[^ ]s", s);  for(i=0;s[i]!=NULL;i++)     {
                count++;
    
        }
        printf("%d",count);
    }

---
+++ characters_in_a_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[200];
+    int count = 0,i,c=0;
+    scanf("%[^
+]s", s);
+ for(i=0;s[i]!=NULL;i++)
+    {
+        
+            count++;
+
+    }
+    printf("%d",count);
+}
