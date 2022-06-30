f8949c0e5023521949d1baadc69056080bb073c4
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-06-30 18:50:09 +0530

    #include<stdio.h> int main() {     int n,i,c,j,d=0,k;     float res,sum=0;     scanf("%d",&n);     int a[100];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf(" %d",&k);     for(i=0;i<n;i++)     { c=0;         if(a[i]!=1 && a[i]!=0)         {         for(j=2;j*j<=a[i];j++)         {             if(a[i]%j==0)             {                 c=1;
                }
            }
            if(c==0)
            {
                sum+=a[i];
                d++;
            }
            }
        }
        res=(sum/d);
        printf("%.2f",res);
    }

---
+++ Average_of_primes.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c,j,d=0,k;
+    float res,sum=0;
+    scanf("%d",&n);
+    int a[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("
+%d",&k);
+    for(i=0;i<n;i++)
+    { c=0;
+        if(a[i]!=1 && a[i]!=0)
+        {
+        for(j=2;j*j<=a[i];j++)
+        {
+            if(a[i]%j==0)
+            {
+                c=1;
+                
+            }
+        }
+        if(c==0)
+        {
+            sum+=a[i];
+            d++;
+        }
+        }
+    }
+    res=(sum/d);
+    printf("%.2f",res);
+}
+
